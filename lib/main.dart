ef9c9cc1b6fe2aef8a9787153e96d8df3e7ddeeb
Srimanta Bagchi <srimantabagchi@gmail.com>
2020-12-27 20:59:34 -0500

    Add icons etc

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: Center(
-    widthFactor: 20,
-    child: Text("Something New"),
-  )));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("I am Rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png')
+          ),
+        ),
+      ),
+    ),
+  );
 }
